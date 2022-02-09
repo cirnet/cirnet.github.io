87959f71c5f8a5d59d104cf056882b828a6d63b0
cirnet <78275621+cirnet@users.noreply.github.com>
2022-02-09 22:01:45 +0100

    https

--- src/app/services/open-weather.service.ts
+++ src/app/services/open-weather.service.ts
@@ -10,7 +10,7 @@ export class OpenWeatherService {
   API_KEY = 'bfc1e1170dde49e4936202756222701';
   getWeather(location: any): any {
     return this.http.get(
-      `http://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${location}&days=5&aqi=no&alerts=no`
+      `https://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${location}&days=5&aqi=no&alerts=no`
     );
   }
 }
