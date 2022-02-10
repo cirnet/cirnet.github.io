594fda9bb82109a1e7d7e9ae25f1abdedcf537df
cirnet <78275621+cirnet@users.noreply.github.com>
2022-02-10 19:37:42 +0100

    index change title

--- src/app/services/open-weather.service.ts
+++ src/app/services/open-weather.service.ts
@@ -7,10 +7,10 @@ export class OpenWeatherService {
   constructor(
     private http: HttpClient
   ) { }
-  API_KEY = 'dcc84956252f2decdfff200fe7dbad17  ';
+  API_KEY = 'dcc84956252f2decdfff200fe7dbad17';
   getWeather(location: any): any {
     return this.http.get(
-      `https://api.weatherapi.com/v1/forecast.json?key=4fe093df7914485aab8210852220902&q=Wrzesnia&days=3&aqi=no&alerts=no      `
+      `https://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${location}&days=3&aqi=no&alerts=no      `
     );
   }
 }
